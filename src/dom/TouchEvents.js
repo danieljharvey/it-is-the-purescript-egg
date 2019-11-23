9d97f6da5e64e7821b1f3154f4e02436de7e2084
Daniel Harvey <danieljamesharvey@gmail.com>
2019-11-23 18:07:39 +0000

    Remove extra log function

--- src/dom/TouchEvents.js
+++ src/dom/TouchEvents.js
@@ -1,7 +1,6 @@
 require("hammerjs");
 
 function setupSwipesActual(elementId, swipeLeftFn, swipeRightFn) {
-  console.log("setupSwipesActual", elementId, swipeLeftFn, swipeRightFn);
   const element = document.getElementById(elementId);
   const hammertime = new Hammer(element, {});
   hammertime.on("swipeleft", function() {
